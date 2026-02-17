9a0db734c5081d454458c7bdc1041c5b899fa34b
hitarthGandhi02 <gandhihitarth02@gmail.com>
2026-02-17 22:45:48 +0530

    fixing deployment issues

--- app/api/class-subjects/route.js
+++ app/api/class-subjects/route.js
@@ -1,29 +1,29 @@
-import { NextResponse } from "next/server";
-import { supabase } from "@/lib/supabaseClient";
+// import { NextResponse } from "next/server";
+// import { supabase } from "@/lib/supabaseClient";
 
-export async function POST(req) {
-  try {
-    const { faculty_id, class_id } = await req.json();
+// export async function POST(req) {
+//   try {
+//     const { faculty_id, class_id } = await req.json();
 
-    const { data, error } = await supabase
-      .from("class_subjects")
-      .select(`
-        subject_id ( id, name )
-      `)
-      .in(
-        "subject_id",
-        supabase
-          .from("faculty_subjects")
-          .select("subject_id")
-          .eq("faculty_id", faculty_id)
-      )
-      .eq("class_id", class_id);
+//     const { data, error } = await supabase
+//       .from("class_subjects")
+//       .select(`
+//         subject_id ( id, name )
+//       `)
+//       .in(
+//         "subject_id",
+//         supabase
+//           .from("faculty_subjects")
+//           .select("subject_id")
+//           .eq("faculty_id", faculty_id)
+//       )
+//       .eq("class_id", class_id);
 
-    if (error) throw error;
+//     if (error) throw error;
 
-    return NextResponse.json({ subjects: data.map((d) => d.subject_id) });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ subjects: data.map((d) => d.subject_id) });
+//   } catch (err) {
+//     console.error(err);
+//     return NextResponse.json({ error: "Server error" }, { status: 500 });
+//   }
+// }
